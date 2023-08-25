d25d6be5c4caa53201180469e668cca21c9e5644
Adam Borowski <adborowski@gmail.com>
2023-08-25 15:07:47 +0200

    created a basic display for regions

--- src/components/region-list.tsx
+++ src/components/region-list.tsx
@@ -1,16 +1,38 @@
-import fs from 'fs'
-import { read, writeFileXLSX } from 'xlsx'
+import { useEffect, useState } from 'react'
+import styles from './regions.module.css'
 
 const RegionList = () => {
-   console.log('Getting region list...')
+   console.log('%cGetting region list...', 'color: yellow')
 
-   fetch('/api/get-regions')
-      .then((res) => res.json())
-      .then((data) => {
-         console.log(data)
-      })
+   interface Region {
+      nr_okregu: number
+      siedziba_okregu: string
+   }
 
-   return <h1>Region list!</h1>
+   const [regionsData, setRegionsData] = useState<Region[]>()
+
+   useEffect(() => {
+      fetch('/api/get-regions')
+         .then((res) => res.json())
+         .then((regionsData) => {
+            if (regionsData) {
+               console.log(regionsData.regions)
+               setRegionsData(regionsData.regions)
+            }
+         })
+   }, [])
+   if (regionsData) {
+      return (
+         <>
+            {regionsData.map((region) => (
+               <div className={styles.region}>
+                  <span>{region.nr_okregu}</span>
+                  <span>{region.siedziba_okregu}</span>
+               </div>
+            ))}
+         </>
+      )
+   }
 }
 
 export default RegionList
--- src/pages/api/get-regions.ts
+++ src/pages/api/get-regions.ts
@@ -6,12 +6,12 @@ var XLSX = require('xlsx')
 
 var workbook = XLSX.readFile('public/okregi.xlsx')
 var sheet_name_list = workbook.SheetNames
-console.log(XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]))
+var regions = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]])
 
 type Data = {
-   name: string
+   regions: {}
 }
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-   res.status(200).json({ name: 'John Doe' })
+   res.status(200).json({ regions: regions })
 }
